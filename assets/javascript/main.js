cd4831162b17d7b48d1300db9d4f1c02bf0211c4
Viet-Vo <viet.allen.qvo@gmail.com>
2018-08-27 23:38:13 -0700

    pretty much done, except the pause and play of gif

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,35 +1,38 @@
-var key = "bd6clIioYnNPIJXuTHnBqevN42Y5O6Gz";
-var numPrint = 2;
 var gifList = ["tree", "sunbear", "rick and morty"];
-var inSearch = "test"
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + inSearch + "&limit=" + numPrint + "&api_key=" + key;
 
+function displayGifCombo() {
+    $("#test").html("test");
+    var key = "bd6clIioYnNPIJXuTHnBqevN42Y5O6Gz";
+    var numPrint = 5;
+    var inSearch = $(this).attr("dataName");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + inSearch + "&limit=" + numPrint + "&api_key=" + key;
 
-$("#test").html("test");
-// "GET" data from api
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-
-    // display info based on
-    for (i = 0; i < response.data.length; i++) {
-        console.log(response.data);
-        var gifDiv = $('<div class="gifs">');
-        var gifRating = response.data[i].rating;
-        var pRating = $("<p>").text("Rating: " + gifRating);
-        gifDiv.append(pRating);
-
-        var gifUrl = response.data[i].images.fixed_height.url;
-        var gifGif = $("<img>");
-        gifGif.attr("src", gifUrl);
-        gifGif.attr("alt", "test");
-        $("#test").append(gifGif);
-    };
-});
+    // "GET" data from api
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // display info based on
+        for (var i = 0; i < response.data.length; i++) {
+            console.log(response.data);
+            var gifDiv = $('<div class="gifHolder">');
+            var gifRating = response.data[i].rating;
+            var pRating = $("<p>").text("Rating: " + gifRating);
+            gifDiv.append(pRating);
+            var gifUrlStill = response.data[i].images.fixed_height_small_still.url;
+            var gifUrl = response.data[i].images.fixed_height_small.url;
+            var gifImg = $("<img>").attr("src", gifUrl);
+            gifImg.attr("data-still", gifUrlStill);
+            gifImg.attr("data-animate", gifUrl);
+            gifImg.attr("data-state", "still")
+            gifImg.attr("class", "gifs");
+            gifDiv.append(gifImg);
+            $("#test").prepend(gifDiv);
+        }
+    });
+};
 
 // function to render button on call
-
 function renderButtons() {
     $("#buttonPlace").empty();
     for (var i = 0; i < gifList.length; i++) {
@@ -40,6 +43,7 @@ function renderButtons() {
         $("#buttonPlace").append(a);
     }
 }
+
 // add new button and push back entry to array
 $("#addGif").on("click", function (event) {
     event.preventDefault();
@@ -48,8 +52,17 @@ $("#addGif").on("click", function (event) {
     renderButtons();
 });
 
+// pause gif
+$(".gifs").on("click", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+});
 
-
-
-
+$(document).on("click", ".gifBtn", displayGifCombo);
 renderButtons();
